e6bc2f8a6b2996b82045c2982b4d27a481f2b3ea
Davide Gavio <gaviodavide@gmail.com>
2020-10-17 10:17:54 +0200

    Added from scratch ridge regression

---
+++ RIdge regression/ridge_regression_from_scratch.py
@@ -0,0 +1,43 @@
+import numpy as np 
+import math
+
+class RidgeRegression(object):
+    """ Base regression model. Models the relationship between a scalar dependent variable y and the independent 
+        variables X. 
+        Parameters:
+        -----------
+        n_iterations: float
+            The number of training iterations the algorithm will tune the weights for.
+        learning_rate: float
+            The step length that will be used when updating the weights."""
+    def __init__(self, n_iterations, learning_rate, reg_factor):
+        self.n_iterations = n_iterations
+        self.learning_rate = learning_rate
+        self.reg_factor = reg_factor
+
+    def initialize_weights(self, n_features):
+        """ Initialize weights randomly [-1/N, 1/N] """
+        limit = 1 / math.sqrt(n_features)
+        self.w = np.random.uniform(-limit, limit, (n_features, ))
+
+    def fit(self, X, y):
+        # Insert constant ones for bias weights
+        X = np.insert(X, 0, 1, axis=1)
+        self.training_errors = []
+        self.initialize_weights(n_features=X.shape[1])
+        # Do gradient descent for n_iterations
+        for i in range(self.n_iterations):
+            y_pred = X.dot(self.w)
+            # Calculate l2 loss
+            mse = np.mean(0.5 * np.subtract(y, y_pred)**2 + self.reg_factor * 0.5 *  self.w.T.dot(self.w))
+            self.training_errors.append(mse)
+            # Gradient of l2 loss w.r.t w
+            grad_w = -(y - y_pred).dot(X) + self.reg_factor * self.w
+            # Update the weights
+            self.w -= self.learning_rate * grad_w
+
+    def predict(self, X):
+        # Insert constant ones for bias weights
+        X = np.insert(X, 0, 1, axis=1)
+        y_pred = X.dot(self.w)
+        return y_pred
